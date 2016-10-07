d317607775e199dbfbdd5a54200be88a0ac57787
Gaile <dpgaile@ad.buffalo.edu>
2016-10-07 17:23:38 -0400

    Added some tidy.R stuff..

---
+++ R/tidy.R
@@ -0,0 +1,22 @@
+#' Represent data in a 'tidy' format
+#'
+#' 'Tidy data takes a 'long' view of the data
+#'
+#' @details This approach popularized by Hadley Wickham;
+#'    see the vignette in the package 'dplyr'
+#'
+#' @importFrom reshape2 melt
+#'
+#' @param pdata_file charactger (1) The path to the pheno data
+#' file
+#'
+#' @param exprs_file character(1) The path to the expression file
+#'
+#' @return A data.frame() with 'tidy' formatting
+#'
+#' @export
+input_tidy <- function(pdata_file, exprs_file){
+  classic <- input_classic(pdata_file,exprs_file)
+  melt(classic, id.vars=1:22, variable.name="probeset",
+       value.name="exprs")
+}
